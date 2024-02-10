03c2575d1b3c4893ad6d96f40cfe5035b77507a9
AkbarezqyBotzz <110279714+AkbarezqyBotzz@users.noreply.github.com>
2024-02-10 12:16:24 +0700

    index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -140,21 +140,7 @@ export const footerLinks = [
     ],
   },
   {
-    title: "Partner",
-    links: [
-      {
-        name: "Our Partner",
-        link: "https://www.hoobank.com/our-partner/",
-      },
-      {
-        name: "Become a Partner",
-        link: "https://www.hoobank.com/become-a-partner/",
-      },
-    ],
-  },
-];
-
-export const socialMedia = [
+    export const socialMedia = [
   {
     id: "social-media-1",
     icon: instagram,
